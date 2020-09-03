215c5b3b6eddbc5e1c8173ddba9599f330f03ad0
Sebastian Alvarez Zapata <sebastianalvarezzapata@Sebastians-MacBook-Pro.local>
2020-09-03 15:58:42 -0500

    First commit.

---
+++ app/src/main/java/com/companytest/thesports/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.companytest.thesports
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
